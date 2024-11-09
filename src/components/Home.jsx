8e0889d3b1b53f75fac6ec0845c1eca166e718ec
mdadilkhan <mdadilakhtar8@gmail.com>
2024-11-09 14:20:11 +0530

    chnged to 3 mont training

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
         <div className=" sm:flex   hidden gap-20 px-16 ">
           <div className="w-full text-left flex items-center gap-1">
             <span className="font-bold text-primary text-[20px] font-sans ">
-              For Post Graduate Students
+              For All Students
             </span>
             <img src={UpArrow} alt="" />
             <hr className="w-[76%] bg-primary h-[2px] " />
@@ -24,7 +24,7 @@ const Home = () => {
         <div className=" flex px-1 w-full sm:hidden ">
           <div className=" text-left flex items-center gap-2  w-full">
             <span className="font-bold text-primary text-[14px] font-sans ">
-            For Post Graduate Students
+            For All Students
             </span>
             <img src={UpArrow} alt="" />
             <hr className="w-[46%] bg-primary h-[2px] " />
@@ -49,7 +49,7 @@ const Home = () => {
               <div className="flex flex-col">
                 <div className="flex gap-20">
                   <p className="text-[16px] text-primary font-light w-1/2">
-                  For Post Graduate Students
+                  For All Students
                   </p>
                   <p className="text-[16px] text-primary font-light w-1/2">
                     Duration : 150 Hours
